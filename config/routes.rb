93c041b5452a46ea424cf92d9a23d52650251ccf
bleckcat <juanvitor16@hotmail.com>
2022-03-02 01:28:12 -0300

    Added entire user session and credentials system

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
   root to: "main#index"
 
+  get "password", to: "passwords#edit", as: :edit_password
+  patch "password", to: "passwords#update"
+
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
+
+  get "sign_in", to: "sessions#new"
+  post "sign_in", to: "sessions#create"
+
+  delete "logout", to: "sessions#destroy"
+
   get "about", to: "about#index"
 end
