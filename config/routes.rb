30f57961f47b90ad6145ecd7ffa47d52d478e70e
bleckcat <juanvitor16@hotmail.com>
2022-03-04 09:23:00 -0300

    Adding twitter account list to user

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,14 @@ Rails.application.routes.draw do
 
   delete "logout", to: "sessions#destroy"
 
+  get "password/reset", to: "password_resets#new"
+  post "password/reset", to: "password_resets#create"
+  get "password/reset/edit", to: "password_resets#edit"
+  patch "password/reset/edit", to: "password_resets#update"
+
+  get '/auth/twitter/callback', to: 'omniauth_callbacks#twitter'
+
+  resources :twitter_accounts
+
   get "about", to: "about#index"
 end
